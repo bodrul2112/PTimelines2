8937f5108c9a0dee1360da3f24665b04aa14ca16
bodrul alam <bodrul2112@gmail.com>
2014-12-22 00:09:29 +0000

    date tile styling
    .

--- src/main/webapp/static/scripts/timeline/data/TimelineData.js
+++ src/main/webapp/static/scripts/timeline/data/TimelineData.js
@@ -21,7 +21,7 @@ define([], function() {
 	          	              {
 	          	            	id: "3",
 	        	            	date: 20150104121212,
-	        	            	textContent: "fourth"
+	        	            	textContent: "fourth Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 	        	              }
 	          	              ]
         				  },
