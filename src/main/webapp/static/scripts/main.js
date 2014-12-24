c7c194fda0eae760459667bb922d59457df5c63d
bodrul alam <bodrul2112@gmail.com>
2014-12-24 16:38:36 +0000

    formatting dates
    using moment.js

--- src/main/webapp/static/scripts/main.js
+++ src/main/webapp/static/scripts/main.js
@@ -1,19 +1,33 @@
 
 
-require( ["plugins/domReady", "thirdparty/jquery", "thirdparty/easing", "thirdparty/jquery-ui", "thirdparty/knockout", "services/TemplateService", "services/SimpleEventHub", "timeline/Timeline"], 
-		function(domReady, jQuery, Easing, jui, knockout, tpl, evt, Timeline){
+require( ["plugins/domReady", 
+          "thirdparty/jquery", 
+          "thirdparty/easing", 
+          "thirdparty/jquery-ui", 
+          "thirdparty/knockout",
+          "thirdparty/moment",
+          "services/TemplateService", 
+          "services/SimpleEventHub", 
+          "timeline/Timeline"], 
+		function(domReady, jQuery, Easing, jui, knockout, mom, tpl, evt, Timeline){
 	
 	domReady(function(){
 		
-		require(["thirdparty/jquery", "thirdparty/easing", "thirdparty/jquery-ui", "thirdparty/knockout", "services/TemplateService", "services/SimpleEventHub", "timeline/Timeline"], 
-				function(jQuery, Easing, jui, knockout, tpl, evt, Timeline) {
+		require(["thirdparty/jquery", 
+		         "thirdparty/easing", 
+		         "thirdparty/jquery-ui", 
+		         "thirdparty/knockout", 
+		         "thirdparty/moment",
+		         "services/TemplateService", 
+		         "services/SimpleEventHub", 
+		         "timeline/Timeline"], 
+				function(jQuery, Easing, jui, knockout, mom, tpl, evt, Timeline) {
 			
 				var oEasing = new Easing(); 
-				
 				KO = knockout;
+				moment.suppressDeprecationWarnings=true;
 				
 				var oTimeline = new Timeline();
-				
 				oTimeline.insertInto($('body'));
 				
 		});
